093ef7574c52cc75423c70b15a72d1fc79e0d7b8
JHWA1102 <94914408+JHWA1102@users.noreply.github.com>
2024-01-15 15:22:49 -0800

    first commit

--- demo/demo/src/main/frontend/src/App.jsx
+++ demo/demo/src/main/frontend/src/App.jsx
@@ -1,3 +1,42 @@
+import React, { Component } from "react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import interactionPlugin from "@fullcalendar/interaction";
+import FullCalendar from '@fullcalendar/react';
+
+export default class DashBoard extends Component {
+    constructor(props){
+        super(props);
+    }
+    dateClick=(info)=>{
+        alert(info.dateStr)
+    }
+    render() {
+        return(
+            <>
+            <div style={{ margin:15, display:'grid',gridTemplateColumns:"2fr 1fr"}}>
+                <FullCalendar   
+                    plugins={[dayGridPlugin,timeGridPlugin,interactionPlugin]}
+                    initialView={'dayGridMonth'}
+                    headerToolbar={
+                        {
+                            start: 'today', 
+                            center: 'title',
+                            end: 'prev,next' 
+                        }
+                    }
+                    height={"85vh"}
+                    dateClick={this.dateClick}
+                    events={[{title:'판매건수 : 23건', date:'2023-05-11',},{title:'판매건수 : 23건',date:'2023-05-13',}]}
+                />
+            </div>
+            </>     
+        );
+    }
+}
+
+
+/*
 import {useEffect, useState} from 'react';
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -27,7 +66,7 @@ function App(){
 }
 
 export default App;
-
+*/
 /*
 import {useEffect, useState} from 'react';
 import reactLogo from './assets/react.svg'
